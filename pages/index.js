1d37b00ad6f63ac46038b80285e1a8d44c19b976
wilyyy <alvarez_william@outlook.com>
2022-02-01 21:39:16 -0800

    theme toggle

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import styled from 'styled-components';
 
+import { useTheme } from '../utils/ScoutThemeProvider';
+
 const Page = styled.div`
   width: 100vw;
   height: 100vh;
@@ -8,8 +10,13 @@ const Page = styled.div`
 `;
 
 const Home = () => {
+  const {theme, setTheme} = useTheme();
   return (
     <Page>
+      <button 
+        onClick={()=>{setTheme(theme === 'dark' ? 'light' : 'dark')}}
+        style={{position: "absolute", right: '2%', top:'2%', width: '100px', height: '50px'}}
+      >test</button>
       asdsad
     </Page>
   )
