044bde5baefe8e4b5e2f40d2f25614153079a35b
Danny Nguyen <dnguyen130@my.bcit.ca>
2022-02-05 15:40:12 -0800

    added empty comps, working on anime card

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
-import Image from 'next/image';
 import styled from 'styled-components';
 
 import { useTheme } from '../utils/ScoutThemeProvider';
+
+import AnimeCard from '../components/AnimeCard';
 
 const Page = styled.div`
   width: 100vw;
@@ -13,11 +14,7 @@ const Home = () => {
   const {theme, setTheme} = useTheme();
   return (
     <Page>
-      <button 
-        onClick={()=>{setTheme(theme === 'dark' ? 'light' : 'dark')}}
-        style={{position: "absolute", right: '2%', top:'2%', width: '100px', height: '50px'}}
-      >test</button>
-      asdsad
+      <AnimeCard />
     </Page>
   )
 }
