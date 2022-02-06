e61e97a5f7da374ec479d7798eaca40fdc7a813a
wilyyy <alvarez_william@outlook.com>
2022-02-05 16:17:42 -0800

    added glass colors and started slider

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,15 @@ import Image from 'next/image';
 import styled from 'styled-components';
 
 import { useTheme } from '../utils/ScoutThemeProvider';
+import MainContentSlider from '../components/MainContentSlider';
 
 const Page = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
   width: 100vw;
-  height: 100vh;
+
   font-family: "Poppins-ExtraLight";
 `;
 
@@ -17,7 +22,7 @@ const Home = () => {
         onClick={()=>{setTheme(theme === 'dark' ? 'light' : 'dark')}}
         style={{position: "absolute", right: '2%', top:'2%', width: '100px', height: '50px'}}
       >test</button>
-      asdsad
+      <MainContentSlider />
     </Page>
   )
 }
