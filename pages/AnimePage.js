90913b85aedb824a0d9b6e657c8176ef51ac2a43
Matt Laurel <alvarez_william@outlook.com>
2022-02-18 15:27:20 -0800

    stuff

--- pages/AnimePage.js
+++ pages/AnimePage.js
@@ -2,9 +2,10 @@
 import styled from "styled-components";
 
 import NavigationBar from "../components/NavigationBar";
+import GlassCard from "../components/GlassCard";
 
 const Page = styled.div`
-    width: 100vw;
+    width: 100%;
     height: 100vh;
     display: flex;
     flex-direction: column;
@@ -16,6 +17,7 @@ const AnimePage = () => {
     return (
         <Page>
             <NavigationBar />
+            <GlassCard>asdas</GlassCard>
         </Page>
     )
 }   
