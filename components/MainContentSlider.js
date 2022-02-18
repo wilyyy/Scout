905c9804f3b4207ff1f19aafae3534e194246568
Danny Nguyen <dt.ngu95@gmail.com>
2022-02-18 15:02:06 -0800

    carousel works, just need to style

--- components/MainContentSlider.js
+++ components/MainContentSlider.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import Image from 'next/image';
+import Tanjiro from '../public/test_demonslayer.jpg';
+
 import { motion } from "framer-motion";
 
 import { CarouselProvider, Slider, Slide } from 'pure-react-carousel';
@@ -7,15 +9,22 @@ import 'pure-react-carousel/dist/react-carousel.es.css';
 
 
 const Container = styled.div`
-    width: 100%;
+    width: 80%;
     height: 556px;
     display: flex;
     flex-direction: column;
+    align-items: center;
     font-family: "Poppins-ExtraLight";
 `;
 
 const CarouselCont = styled.div`
-    width: 100%;
+    width: 80%;
+    background-color: rgba(0,0,0,0.2);
+    min-height: 494px;
+    max-height: 494px;
+    display: flex;
+    justify-content: center;
+
 `
 
 
@@ -24,13 +33,22 @@ const MainContentSlider = () => {
         <Container>
             <CarouselCont>
             <CarouselProvider
-                naturalSlideWidth={100}
-                naturalSlideHeight={125}
-                totalSlides={3}>
-                <Slider>
-                    <Slide index={0}>I am the first Slide.</Slide>
-                    <Slide index={1}>I am the second Slide.</Slide>
-                    <Slide index={2}>I am the third Slide.</Slide>
+                naturalSlideWidth={1000}
+                naturalSlideHeight={494}
+                totalSlides={3}
+                infinite={true}
+                interval={500}
+                isPlaying={true}>
+                <Slider className="slider">
+                    <Slide index={0} className="slide">
+                        <Image src={Tanjiro} height="556px" width="1000px" />
+                    </Slide>
+                    <Slide index={1} className="slide">
+                        <Image src={Tanjiro} height="556px" width="1000px" />
+                    </Slide>
+                    <Slide index={2} className="slide">
+                        <Image src={Tanjiro} height="556px" width="1000px" />
+                    </Slide>
                 </Slider>
             </CarouselProvider>
             </CarouselCont>
