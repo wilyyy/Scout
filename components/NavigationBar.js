dee727be99205f4a5bbe527c99f45e61033e2361
wilyyy <alvarez_william@outlook.com>
2022-02-19 01:48:55 -0800

    unfin nav bar, dont think we should use glasscard

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -2,13 +2,17 @@ import Link from "next/link";
 import styled from "styled-components";
 import { SunFill } from '@styled-icons/bootstrap/SunFill';
 import { MoonStarsFill } from '@styled-icons/bootstrap/MoonStarsFill';
+import { Filter } from '@styled-icons/ionicons-solid/Filter';
 import ReactSwitch from "react-switch";
 import { motion } from "framer-motion";
 import { useRouter } from 'next/router';
+import Image from "next/image";
 
 import { useTheme } from '../utils/ScoutThemeProvider';
 import { ThemeConfig } from "../utils/ThemeConfig";
 import { HoverZoom } from "../utils/Animations";
+import { DarkColors, LightColors } from "../utils/ThemeConfig";
+import SearchBar from "./SearchBar";
 
 const Container = styled.div`
     width: 100%;
@@ -23,12 +27,21 @@ const Container = styled.div`
     }
 `;
 
-const LinkCont = styled.div`
-    width: 15%;
-    height: 54px;
+const Row = styled.div`
+    ${({links})=> links && `
+        width: 15%;
+        height: 54px;
+        justify-content: space-between;
+    `}
+
+    ${({right})=> right && `
+        width: 45%;
+        height: 75px;
+        justify-content: flex-end;
+    `}
+
     font-family: "Poppins-ExtraLight";
     display: flex;
-    justify-content: space-between;
     align-items: center; 
     
     a:hover{
@@ -37,18 +50,35 @@ const LinkCont = styled.div`
 `;
 
 const SunIcon = styled(SunFill)`
-    width: 35px;
-    height: 35px;
-    color: ${props=>props.color};
+    width: 20px;
+    height: 20px;
+    margin-left: 5px;
 `;
 
 const MoonIcon = styled(MoonStarsFill)`
-    width: 35px;
-    height: 35px;
-    color: ${props=>props.color};
+    width: 20px;
+    height: 20px;
+    margin-left: 5px;
 `;
 
-const NavigationBar = ({onHomeClick, onFavouritesClick}) => {
+const FilterIcon = styled(Filter)`
+    width: 40px;
+    height: 26px;
+`;
+
+const AvatarCont = styled(motion.div)`
+    width: 75px;
+    height: 75px;
+    border-radius: 100px;
+    overflow: hidden;
+`;
+
+const NavigationBar = ({
+    onHomeClick, 
+    onFavClick, 
+    onProfileClick,
+    onFilterClick
+}) => {
     const router = useRouter();
     const {theme, setTheme} = useTheme();
 
@@ -62,7 +92,7 @@ const NavigationBar = ({onHomeClick, onFavouritesClick}) => {
             >
                 Scout
             </motion.h1>
-            <LinkCont>
+            <Row links>
                 <motion.a 
                     whileHover={HoverZoom.hover}
                     whileTap={HoverZoom.tap}
@@ -75,15 +105,36 @@ const NavigationBar = ({onHomeClick, onFavouritesClick}) => {
                     whileHover={HoverZoom.hover}
                     whileTap={HoverZoom.tap}
                     transition={HoverZoom.spring}
-                    onClick={onHomeClick}
+                    onClick={onFavClick}
                 >
                     Your List
                 </motion.a>
-            </LinkCont>
-            <ReactSwitch 
-                onChange={()=>{setTheme(theme === 'dark' ? 'light' : 'dark')}}
-                checked={theme === 'dark' ? false : true}
-            />
+            </Row>
+            <Row right>
+                <SearchBar onSearchClick={()=>console.log("hello")}/>
+                <FilterIcon onClick={onFilterClick} />
+                <ReactSwitch
+                    onChange={()=>{setTheme(theme === 'dark' ? 'light' : 'dark')}}
+                    checked={theme === 'dark' ? false : true}
+                    offColor = "#FFf"
+                    offHandleColor = {DarkColors.Pumpkin}
+                    onColor = "#1E1E1E"
+                    onHandleColor ={LightColors.Denim}
+                    activeBoxShadow="0px 0px 1px 2px #fffc35"
+                    uncheckedIcon={<SunIcon />}
+                    checkedIcon={<MoonIcon />}
+                />
+                <AvatarCont
+                    whileHover={HoverZoom.hover}
+                    whileTap={HoverZoom.tap}
+                    transition={HoverZoom.spring}
+                    onClick={onProfileClick}
+                >
+                    <img
+                        src="https://placekitten.com/100/100"
+                    />
+                </AvatarCont>
+            </Row>
         </Container>
     )
 }
